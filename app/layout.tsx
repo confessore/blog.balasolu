43607baa28c1679dfb0b5434acf36a6271fc8bff
confessore <confessore@balasolu.com>
2023-08-20 13:59:31 -0400

    nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
             <header>
               <div className="flex items-center justify-between">
                 <nav className="ml-auto space-x-6 text-sm font-medium">
-                  <Link href="/">Home</Link>
-                  <Link href="/about">About</Link>
+                  <a href="https://balasolu.com">Home</a>
+                  <Link href="/">Blog</Link>
                 </nav>
               </div>
             </header>
