f2beaa5a7bb16cc5681c39549031439697d64131
confessore <confessore@balasolu.com>
2023-08-20 12:39:47 -0400

    v

--- components/main_wrapper.tsx
+++ components/main_wrapper.tsx
@@ -7,7 +7,7 @@ export default function MainWrapper({
 }) {
   return (
     <main className="flex min-h-screen min-w-full flex-col items-center justify-between gap-3 text-slate-950 dark:text-slate-50">
-      <div className="fixed inset-0 -z-10 h-full w-full">
+      <div className="fixed inset-0 -z-10 h-[100vh] w-[100vw]">
         <Image
           src={"https://balasolu.com/jwt.webp"}
           alt={"jwt"}
