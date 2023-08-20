6cfe8bf5b0cc0521050c8e9408728bd71b9b4ec1
confessore <confessore@balasolu.com>
2023-08-20 15:21:07 -0400

    package scam

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="prose dark:prose-invert">
-      {allPosts.map((post) => (
+      {allPosts.sort((x, y) => Date.parse(y.date) - Date.parse(x.date)).map((post) => (
         <article key={post._id}>
           <Link href={post.slug}>
             <h2>{post.title}</h2>
