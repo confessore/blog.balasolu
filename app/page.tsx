da6312dbc4b4da5f0ead371551ecc911fbfcd99c
confessore <confessore@balasolu.com>
2023-08-21 12:08:22 -0400

    format

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,16 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="prose dark:prose-invert">
-      {allPosts.sort((x, y) => Date.parse(y.date) - Date.parse(x.date)).map((post) => (
-        <article key={post._id}>
-          <Link href={post.slug}>
-            <h2>{post.title}</h2>
-          </Link>
-          {post.description && <p>{post.description}</p>}
-        </article>
-      ))}
+      {allPosts
+        .sort((x, y) => Date.parse(y.date) - Date.parse(x.date))
+        .map((post) => (
+          <article key={post._id}>
+            <Link href={post.slug}>
+              <h2>{post.title}</h2>
+            </Link>
+            {post.description && <p>{post.description}</p>}
+          </article>
+        ))}
     </div>
   );
 }
--- components/main_wrapper.tsx
+++ components/main_wrapper.tsx
@@ -6,7 +6,7 @@ export default function MainWrapper({
   children: React.ReactNode;
 }) {
   return (
-    <main className="flex min-h-screen min-w-full flex-col items-center gap-3 py-10 text-slate-950 bg-slate-50 opacity-90 dark:text-slate-50 dark:bg-slate-950 lg:py-0 lg:pb-10">
+    <main className="flex min-h-screen min-w-full flex-col items-center gap-3 bg-slate-50 py-10 text-slate-950 opacity-90 dark:bg-slate-950 dark:text-slate-50 lg:py-0 lg:pb-10">
       {children}
     </main>
   );
