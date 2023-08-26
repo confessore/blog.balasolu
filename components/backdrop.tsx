e2cec454f2573e4bb9c56ef3787ef92b233577a4
confessore <confessore@balasolu.com>
2023-08-26 16:31:58 -0400

    overflow

--- components/backdrop.tsx
+++ components/backdrop.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Backdrop({ children }: { children: React.ReactNode }) {
   return (
-    <div className="items-between mx-auto flex h-full w-full max-w-2xl flex-col flex-wrap justify-between bg-slate-200 p-8 px-4 opacity-90 dark:bg-slate-900">
+    <div className="items-between mx-auto flex h-full w-full max-w-5xl overflow-x-auto flex-col flex-wrap justify-between bg-slate-200 p-3 opacity-90 dark:bg-slate-900">
       {children}
     </div>
   );
--- components/overlay.tsx
+++ components/overlay.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Overlay() {
   return (
-    <div className="z-10 w-full max-w-5xl items-center justify-between p-8 font-mono text-sm lg:flex">
+    <div className="z-10 w-full max-w-5xl items-center justify-between p-3 font-mono text-sm lg:flex">
       <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
         Feel free to call or text&nbsp;
         <code className="font-mono font-bold">
@@ -12,7 +12,7 @@ export default function Overlay() {
       </p>
       <div className="fixed bottom-0 left-0 flex h-20 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
         <Link
-          className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+          className="pointer-events-none flex place-items-center gap-2 p-3 lg:pointer-events-auto lg:p-0"
           href="/"
           rel="noopener noreferrer"
         >
